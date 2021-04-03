dde3f3daeaad5996569e8ebd739b369eebc0c867
Regina Nosova <jrokku43@gmail.com>
2021-04-03 17:03:27 +0300

    task.js changed

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -5,23 +5,32 @@
 	3) console.log() прописывать не обязательно, т.к. команда return уже означает вывод результата работы функции.
  */
 
+
 function averageMark(a, g, p) {
+	// реализуйте здесь задание №1
+	let algebra = a;
+	let geography = g;
+	let physics = p;
 	
-    // реализуйте здесь задание №1
+    let finalGrade = (algebra + geography + physics) / 3;
     
-	return // ваш ответ;
+	return finalGrade;// ваш ответ;
 }
 
 function sayHello(userName) {
-	
-    // реализуйте здесь задание №2
-    
-	return // ваш ответ;
+	// реализуйте здесь задание №2
+	let myName = userName;
+    let message = `Привет, мир! Меня зовут ${myName}.`;
+	return message;// ваш ответ;
 }
 
 function calculateFormula() {
-    
 	// реализуйте здесь задание №3
+	let x = 2;
+	let y = 22;
+	let z = 0;
+
+	let result = x * y + 5 * z + x - 1;
     
-    return // ваш ответ;
+    return result; // ваш ответ;
 }
