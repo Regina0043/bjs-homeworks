e24fbe938f072d1cf757f9032767239c39dcdb95
Regina Nosova <jrokku43@gmail.com>
2021-04-09 17:52:48 +0300

    Mistakes corrected

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,66 +1,49 @@
 'use strict';
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
+    // код для задачи №1 писать здесь 
     // return x;
- let d = b**2 - 4 * a * c;
+ 	let d = b**2 - 4 * a * c;
 
- let x;
+ 	let x = [];
 
- if(d < 0){
- 	x = [];
- 	return x;
- }
+ 	if (d === 0){
+ 		x.push((-b + Math.sqrt(d)) / (2 * a));
+ 		console.log(x);
+ 
+ 	}
 
- else if(d === 0){
- 	x = [(-b - c) / a];
- 	return x;
- }
+ 	else if (d > 0){
+ 		x.push((-b - Math.sqrt(d)) / (2 * a));
+ 		x.push((-b + Math.sqrt(d)) / (2 * a));
+ 		console.log(x);
+ 	}
 
- else if (d > 0){
- 	x = [(-b - c) / a, (b + c) / a];
- 	return x;
- }
+ 	return x; 
 
 }
 
 function getAverageMark(marks){
     // код для задачи №2 писать здесь
     // return averageMark;
-	
+	let sum = 0;
+	let averageMark;
 
-		let sum = 0;
-		let averageMark;
+	let newArr = marks.slice(0, 5);
 
-		if (marks.length === 0){
-			return 0;
-		}
+	for (let i = 0; i < newArr.length; i++){
+		sum += newArr[i];
+		averageMark = sum / newArr.length;
+	}
 
-		else if(marks.length > 5){
-			let newArr = marks.slice(0, 5);
-			for (let i = 0; i < newArr.length; i++){
-			
-				sum += newArr[i];
-				averageMark = sum / newArr.length;
-				console.log("Количество оценок больше 5-ти");
-				console.log(newArr.length);
-				console.log(averageMark);
-				console.log(sum);
+	if (marks.length === 0){
+		return 0;
+	}
+
+	else if(marks.length > 5){
+		console.log('Количество оценок больше 5-ти');
+	}
 		
-			}
-			return averageMark;
-		}
-
-		else {
-			for(let i = 0; i < marks.length; i++){
-				sum += marks[i];
-				averageMark = sum / marks.length;
-			}
-			return averageMark;
-
-		}
-
-		
-
+	return averageMark;
 	
     
 }
@@ -70,16 +53,12 @@ function askDrink(name,dateOfBirthday){
     // return result;
 	let result;
 
-	let today = new Date;
-
-	
-
-    if(today.getFullYear() - dateOfBirthday.getFullYear() >= 18){
+    if(new Date().getFullYear() - dateOfBirthday.getFullYear() >= 18){
     	result = `Не желаете ли олд-фэшн, ${name} ?`;
     	return result;
     }
 
-    else if (today.getFullYear() - dateOfBirthday.getFullYear() < 18){
+    else if (new Date().getFullYear() - dateOfBirthday.getFullYear() < 18){
     	result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`;
     	return result;
     }
